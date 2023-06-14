da9c27bdf9498dd6147937ec9d36445e8ef0b756
bluesskywhiteclouds <134117505+bluesskywhiteclouds@users.noreply.github.com>
2023-06-14 16:19:59 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'hong',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'hong个人网站',
 
   // social usernames (optional)
    // twitter: 'chusight1',
