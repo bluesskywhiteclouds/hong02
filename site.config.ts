9e8d7bf0a9d2b2870b7d2d06b15b86eacbc066ab
bluesskywhiteclouds <134117505+bluesskywhiteclouds@users.noreply.github.com>
2023-06-14 16:14:47 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'o-id',
+  rootNotionSpaceId: 'hong',
 
   // basic site info (required)
   name: 'hong',
@@ -53,12 +53,12 @@ export default siteConfig({
      navigationStyle: 'custom',
      navigationLinks: [
        {
-         title: 'About',
-         pageId: '2c53271e47464ca59f021907db220508'
+         title: 'Resume',
+         pageId: '487a902ca2044b47b2467917b1589296'
        },
        {
-         title: 'Contact',
-         pageId: 'b8b7971e749b41f487a1d00c40eb7a0d'
+         title: 'Article',
+         pageId: '2b0cd479d5e948dc9f9530ab0b3e5ffb'
        }
      ]
 })
